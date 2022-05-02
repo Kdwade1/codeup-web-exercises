2f2cdd5b5e683237265e0b10d6e849dba2c47e35
Kelvin <kelvin.daquon.wade@gmail.com>
2022-05-02 12:22:04 -0500

    done

--- js/warmup.js
+++ js/warmup.js
@@ -32,3 +32,15 @@ function inorder(x){
     return x.split("").sort().join('')
 }
 console.log(inorder("Codeup"))
+
+
+function sumOfElements(arr){
+    let sum=0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+
+    }
+    return sum
+
+}
+console.log(sumOfElements([1,2,3,4,5]));
