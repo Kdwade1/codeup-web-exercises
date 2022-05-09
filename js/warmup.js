e1236e9b86e8da268194c8df6eb8ab3275c948af
Kelvin <kelvin.daquon.wade@gmail.com>
2022-05-09 16:23:40 -0500

    update

--- js/warmup.js
+++ js/warmup.js
@@ -59,4 +59,54 @@ let product1 = {
     description: 'It is a a hammar.',
     inventory: 25034
 }
+
+
 console.log(returnProductDetail(product1));
+
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function largestHeight(arr){
+    let tallest ={heightInMM: 0}
+    for(let hamster of arr){
+        if (hamster.heightInMM> tallest.heightInMM){
+            tallest= hamster
+        }
+    }
+    return tallest;
+
+}
+
+console.log(largestHeight(hamsters))
