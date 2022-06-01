8499acfd7e5e9fac342440125a8750ff2bb4fdb9
Kelvin <kelvin.daquon.wade@gmail.com>
2022-06-01 16:41:49 -0500

    update

--- js/weather-app.js
+++ js/weather-app.js
@@ -12,25 +12,29 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     lon: -98.48527,
     units: "imperial"
 }).done(function (data) {
-    console.log('The entire response:', data);
-    console.log('Diving in - here is current information: ', data.current);
-    console.log('A step further - information for tomorrow: ', data.daily[1]);
-    console.log("One step even further: ", data.daily[2]);
+
+    // "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png"
     data.daily.forEach(function (day, i) {
         if (i < 5) {
-            let section= "<section>"
-            let section2= "</section>"
-            let hr="<hr>"
-            let div = "<div>" + day.temp.max + "</div>"
-            let div1 = "<div>" + day.humidity + "</div>"
-            let div2 = "<div>" + day.weather[0].description + "</div>"
-            let div3 = "<div>" + day.pressure + "</div>"
-            let div4 = "<div>" + day.wind_speed + "</div>"
-            $(".container").append(section+div2 +hr+ div +hr+ div1 +hr+ div3 +hr+ div4+section2);
-            console.log(day)
+            let section = "<section class='cardbody'>"
+            let section2 = "</section>"
+            let timestamp = day.dt
+
+            let divvy = "<div>"
+            let divn = "</div>"
+            let date = new Date(timestamp * 1000)
+            let img = "<img src=http://openweathermap.org/img/w/" + day.weather[0].icon + ".png>"
+            let div = "<div>" + "Temperature:  " +day.temp.min+"°F /"+day.temp.max +"°F"+ "</div>"
+            let div1 = "<div >"+"Humidity:  " + day.humidity + "</div>"
+            let div2 = "<div>" + "Description: "+day.weather[0].description + "</div>"
+            let div3 = "<div>" +"Pressure: "+ day.pressure + "</div>"
+            let div4 = "<div>" + "Wind: "+day.wind_speed + "</div>"
+            $(".container").append(section +img+   div2+divvy + date + divn   + div + div1 + div3 + div4 + section2);
+
         }
     })
     $("section").addClass("card")
+
 // $.get("http://api.openweathermap.org/data/2.5/onecall").done(function(data){
 //     // $(".container").html()
 //     console.log(data)
