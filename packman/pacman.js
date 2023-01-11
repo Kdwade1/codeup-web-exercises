f784e744b47033af8975da911024a10f623820b0
Kelvin <kelvin.daquon.wade@gmail.com>
2023-01-11 15:49:03 -0600

    adding boundaries

--- packman/pacman.js
+++ packman/pacman.js
@@ -1,1 +1,47 @@
-const canvas =document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext('2d')
+console.log(c)
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Boundary {
+    constructor({position}) {
+        this.position = position
+        this.width = 40;
+        this.height = 40;
+    }
+
+    draw() {
+        c.fillStyle = "blue";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-']
+]
+const boundaries = []
+map.forEach((row) => {
+    row.forEach((symbol) => {
+        switch (symbol) {
+            case'-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: 0,
+                            y: 0
+                        }
+
+
+                    })
+                )
+                break;
+        }
+    })
+})
+boundaries.forEach(boundary => {
+    boundary.draw()
+})
