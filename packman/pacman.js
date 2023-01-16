097dfa1a035e94d7c0358ef2a4ccbfc9ee240761
Kelvin <kelvin.daquon.wade@gmail.com>
2023-01-15 22:34:39 -0600

    updated pacman

--- packman/pacman.js
+++ packman/pacman.js
@@ -20,6 +20,8 @@ class Boundary {
     }
 }
 
+//create pacman and his movemnts
+
 class PacMan {
     constructor({position, velocity}) {
         this.position = position
@@ -35,10 +37,11 @@ class PacMan {
         c.fill()
         c.closePath();
     }
-    update(){
-this.draw();
-this.position.x+=this.velocity.x
-this.position.y+=this.velocity.y
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
     }
 }
 
@@ -46,29 +49,30 @@ this.position.y+=this.velocity.y
 const boundaries = []
 const pacMan = new PacMan({
     position: {
-        x: Boundary.width+ Boundary.width/2,
-        y:Boundary.height + Boundary.height/2
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
     },
-    velocity:{
-        x:0,
-        y:0
+    velocity: {
+        x: 0,
+        y: 0
     }
 });
-const keys={
-    w:{
-        pressed:false
+const keys = {
+    w: {
+        pressed: false
     },
-    a:{
-        pressed:false
+    a: {
+        pressed: false
     },
-    s:{
-        pressed:false
+    s: {
+        pressed: false
     },
-    d:{
-        pressed:false
+    d: {
+        pressed: false
     }
 }
-let lastKey =''
+let lastKey = ''
+//the arena the game follows
 const map = [
     ['-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
@@ -94,62 +98,87 @@ map.forEach((row, i) => {
         }
     })
 })
-function animate(){
-    requestAnimationFrame(animate)
-    c.clearRect(0,0,canvas.width,canvas.height)
-    boundaries.forEach(boundary => {
-        boundary.draw()
+function circleCollidesWithRectangle({circle, rectangle}) {
+    return (
+        circle.position.y - circle.radius + circle.velocity.y <= rectangle.position.y + rectangle.height && circle.position.x + circle.radius + circle.velocity.x >= circle.position.x && circle.position.y + circle.radius + circle.velocity.y >= rectangle.position.y && circle.position.x - circle.radius + circle.velocity.x <= rectangle.position.x + rectangle.width
+    )
+
+
+    }
+
+//pacman movements and animations
+    function animate() {
+        requestAnimationFrame(animate)
+        c.clearRect(0, 0, canvas.width, canvas.height)
+
+        if (keys.w.pressed && lastKey === 'w') {
+            pacMan.velocity.y = -5
+        } else if (keys.s.pressed && lastKey === 's') {
+            pacMan.velocity.y = 5
+        } else if (keys.a.pressed && lastKey === 'a') {
+            pacMan.velocity.x = -5
+        } else if (keys.d.pressed && lastKey === 'd') {
+            pacMan.velocity.x = 5
+        }
+        boundaries.forEach(boundary => {
+            boundary.draw()
+            // checks collision
+            if (circleCollidesWithRectangle({
+                circle:pacMan,
+                rectangle: boundary
+            })
+            ){
+                pacMan.velocity.y = 0
+                pacMan.velocity.x = 0
+            }
+
+        })
+        pacMan.update()
+        // pacMan.velocity.y = 0
+        // pacMan.velocity.x = 0
+
+    }
+
+    animate()
+
+
+    window.addEventListener('keydown', ({key}) => {
+
+        switch (key) {
+            case'w':
+                keys.w.pressed = true
+                lastKey = 'w'
+                break;
+            case's':
+                keys.s.pressed = true
+                lastKey = 's'
+                break;
+            case'a':
+                keys.a.pressed = true
+                lastKey = 'a'
+                break;
+            case'd':
+                keys.d.pressed = true;
+                lastKey = 'd'
+                break;
+        }
 
     })
-    pacMan.update()
-    pacMan.velocity.y=0
-    pacMan.velocity.x=0
+    window.addEventListener('keyup', ({key}) => {
 
-    if(keys.w.pressed && lastKey=== 'w'){
-        pacMan.velocity.y=-5
-    }else if(keys.s.pressed&& lastKey=== 's'){
-        pacMan.velocity.y =5
-    }else if(keys.a.pressed&& lastKey=== 'a'){
-        pacMan.velocity.x =-5
-    }else if(keys.d.pressed&& lastKey=== 'd'){
-        pacMan.velocity.x =5
-    }
-}
-animate()
+        switch (key) {
+            case'w':
+                keys.w.pressed = false;
+                break;
+            case's':
+                keys.s.pressed = false;
+                break;
+            case'a':
+                keys.a.pressed = false;
+                break;
+            case'd':
+                keys.d.pressed = false;
+                break;
+        }
 
-
-
-window.addEventListener('keydown',({key})=>{
-
-    switch(key){
-        case'w':
-            keys.w.pressed=true
-            lastKey ='w'
-        break;
-        case's': keys.s.pressed=true
-            lastKey ='s'
-        break;
-        case'a': keys.a.pressed=true
-            lastKey ='a'
-        break;
-        case'd': keys.d.pressed=true;
-            lastKey ='d'
-        break;
-    }
-
-})
-window.addEventListener('keyup',({key})=>{
-
-    switch(key){
-        case'w':
-            keys.w.pressed=false;
-            break;
-        case's': keys.s.pressed=false;
-            break;
-        case'a': keys.a.pressed=false;
-            break;
-        case'd': keys.d.pressed=false;
-            break;
-    }
-
-})
+    })
